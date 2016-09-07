e18fa9cf4f640d871c61543c5046f372bbc1e68a
DartNyan <DwightNyan@Gmail.com>
2016-09-07 14:48:01 +0300

    Add orders conroller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,43 @@
 class OrdersController < ApplicationController
+
+	before_action :set_order, only: [:create, :destroy, :edit, :update]
+
+	def new
+		@order = Order.new
+	end
+
+	def create
+		if @order.save
+      redirect_to root, :notice => 'Спасибо за ваш заказ!!'
+    else
+      render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		@order.update_attributes (order_params)
+	end
+
+	def index
+		# @phone = Order.find_by(phone_number: params[:phone_number])
+		@orders = Order.all.find_by(phone_number: params[:phone_number])
+	end
+
+	def destroy
+		@order.destroy
+	end
+
+	private
+
+		def order_params
+			params.require(:order).permit(:username, :phone_number, :adress_from, :adress_to)
+		end
+
+		def set_order
+      @order = Order.find params[:id]
+    end
+
 end
